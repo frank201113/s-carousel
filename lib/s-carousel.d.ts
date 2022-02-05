3ee6be7cf6f3be6cc66707b6d4ff99f174d9f097
frank201113 <1831328181@qq.com>
2022-02-05 22:15:10 +0800

    rebuild

--- lib/s-carousel.d.ts
+++ lib/s-carousel.d.ts
@@ -1,3 +1,11 @@
-import { SCarouselInstance, SCarouselProps } from "./carousel-type";
-declare function SCarousel(this: SCarouselInstance, opts: SCarouselProps): void;
+import { SCarouselProps } from "./carousel-type";
+declare class SCarousel {
+    start: () => void;
+    stop: () => void;
+    continue: () => void;
+    go: (target: number | string) => void;
+    resize: (width: number, height: number) => void;
+    destroy: () => void;
+    constructor(opts: SCarouselProps);
+}
 export default SCarousel;
